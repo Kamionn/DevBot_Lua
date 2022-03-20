68456212456d032d0677be03cd22d30e43b9d653
Kamionn <88730423+Kamionn@users.noreply.github.com>
2022-03-20 11:31:34 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 local discordia = require('discordia')
 local client = discordia.Client()
-local prefix = "."
- 
+
 client:on('ready', function()
 	print('Logged in as '.. client.user.username)
 end)
